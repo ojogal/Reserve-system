d688e71d0100a14a52bf6d86cd0e08108a4c8286
Alex <ojog.al@gmail.com>
2022-10-10 12:54:07 +0300

    Adds destroy action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,6 +21,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    head 204
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password)
