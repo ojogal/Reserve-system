d76dac38a090d8cb4edd105b24344e91bed24e1e
Alex <ojog.al@gmail.com>
2022-10-10 12:34:56 +0300

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api , defaults: { format: :json } do
     namespace :vi do
-
+      resources :users, only: %i[:show :create :update :destroy]
     end
   end
 end
