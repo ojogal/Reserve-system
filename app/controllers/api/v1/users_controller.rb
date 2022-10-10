3c01765d45db7ff5549e1c5447a343e123bba5f1
Alex <ojog.al@gmail.com>
2022-10-10 12:37:19 +0300

    Adds update action to the users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::UsersController < ApplicationController
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
