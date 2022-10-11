db2927a284b88b5f5a79905a20646a279072d9c4
Alex <ojog.al@gmail.com>
2022-10-11 13:22:39 +0300

    Create a seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+Table.delete_all
 User.delete_all
 
 u1 = User.create! email: 'user@mail.com', password: '1111'
+puts "Created a new user: #{u1.email}"
 
 t1 = Table.create! table: '1', seats: '2', user_id: u1.id
+puts "Created a new table: #{t1.table}"
